74745a406660163714ce208939f4427677b5579a
Francesco W <fwanderlingh@gmail.com>
2021-11-25 15:28:44 +0100

    Comment correction 2

--- include/GetBasicVectorWrtBase.m
+++ include/GetBasicVectorWrtBase.m
@@ -1,6 +1,6 @@
 %% GetBasicVectorWrtBase function 
 % inputs :
-% - biTei: vector of matrices containing the transformation matrices from link i to link i +1. The
+% - biTei: vector of matrices containing the transformation matrices of link <i> w.r.t. link <i-1>. The
 % size of biTri is equal to (4,4,numberOfLinks)
 % - linkNumber: number of link for which computing the basic vector from the
 % base to the joint
